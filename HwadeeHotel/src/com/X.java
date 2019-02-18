261ac0ce2dc3bcf924fe1a1c3699ab40e944fd87
QZ <1578166061@qq.com>
2019-02-18 23:15:25 +0800

    comment dev

--- HwadeeHotel/src/com/X.java
+++ HwadeeHotel/src/com/X.java
@@ -3,7 +3,7 @@ package com;
 public class X {
 	public static void main(String args[]){
 		System.out.println("hello the cruel world.");
-		System.out.println("bye!");
+		System.out.println("re fuck!");
 	}
 }
 		
